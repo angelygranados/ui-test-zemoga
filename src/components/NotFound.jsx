c7ccb179b6294c3ee846036ae6fe651fa47406d5
angelygranados <angelinagranados@gmail.com>
2020-01-03 18:48:56 -0500

    NotFound

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/NotFound.scss';
+
+const NotFound = () => (
+    <section class="pageNotFound">
+        <h2 class="pageNotFound-title">404</h2>
+        <span class="pageNotFound-text">Página no encontrada</span>
+    </section>
+);
+
+export default NotFound;
