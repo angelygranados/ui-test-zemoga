98d271b52ba1077cc78253e092274e741fdb7db1
angelygranados <angelinagranados@gmail.com>
2020-01-03 18:29:55 -0500

    not found

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,10 +8,10 @@ const Menu = () => (
         <div className="menu__title">Rule of Thumb.</div>
         <div className="menu__navbar">
             <ul>
-                <li className="menu__navbar--item"><Link to="/" className="item__link">Past Trials</Link></li>
-                <li className="menu__navbar--item"><Link to="/" className="item__link">How It Works</Link></li>
-                <li className="menu__navbar--item"><Link to="/" className="item__link">Log In / Sign Up </Link></li>
-                <li className="menu__navbar--item"><Link to="/" className="item__link"><img className="item__search" src={searchIcon} alt="search"/></Link></li>
+                <li className="menu__navbar--item"><Link to="/trials" className="item__link">Past Trials</Link></li>
+                <li className="menu__navbar--item"><Link to="/works" className="item__link">How It Works</Link></li>
+                <li className="menu__navbar--item"><Link to="/login" className="item__link">Log In / Sign Up </Link></li>
+                <li className="menu__navbar--item"><Link to="/search" className="item__link"><img className="item__search" src={searchIcon} alt="search"/></Link></li>
             </ul>
             
         </div>
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../containers/Home';
+import NotFound from '../components/NotFound';
 const App = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}/>
-            {/* <Route component={NotFound}/> */}
+            <Route component={NotFound}/>
         </Switch>
     </BrowserRouter>
 );
